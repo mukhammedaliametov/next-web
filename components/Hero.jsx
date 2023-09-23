8b181bb34c1c0fe0d9aaeda22e1c4b27cd645418
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-09-23 12:25:19 +0500

    second commit ;)

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = ({heading, message}) => {
   return (
-    <div className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img'>
+    <div className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img font-raleway'>
       {/* Overlay */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/60 h-full' />
       <div className='p-5 z-[2] text-white sm:ml-[-25rem] mt-[6rem]'>
--- components/Slider.jsx
+++ components/Slider.jsx
@@ -6,27 +6,22 @@ const Slider = () => {
     {
       url: "https://images.unsplash.com/photo-1535224206242-487f7090b5bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
       title: 'Nature Image 1',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint qui quod iste iure incidunt, id labore natus distinctio sequi repellat!'
     },
     {
       url: "https://images.unsplash.com/photo-1469362102473-8622cfb973cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2419&q=80",
       title: 'Nature Image 2',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint qui quod iste iure incidunt, id labore natus distinctio sequi repellat!'
     },
     {
       url: "https://images.unsplash.com/photo-1594717527389-a590b56e8d0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
       title: 'Nature Image 3',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint qui quod iste iure incidunt, id labore natus distinctio sequi repellat!'
     },
     {
       url: "https://images.unsplash.com/photo-1484591974057-265bb767ef71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
       title: 'Nature Image 4',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint qui quod iste iure incidunt, id labore natus distinctio sequi repellat!'
     },
     {
       url: "https://images.unsplash.com/photo-1545917992-dea2d782ef46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2489&q=80",
       title: 'Nature Image 5',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint qui quod iste iure incidunt, id labore natus distinctio sequi repellat!'
     },
   ];
 
@@ -46,7 +41,7 @@ const Slider = () => {
 
   return (
     <div className="max-w-[1400px] h-[780px] w-full m-auto p-16 px-4 relative group">
-      <h1 className="text-center text-3xl font-bold p-4">Gallery</h1>
+      <h1 className="text-center text-3xl p-4 font-mooli">Gallery</h1>
       <div
         style={{ background: `url(${slides[currentIndex].url})` }}
         className="w-full h-[100%] rounded-2xl bg-center bg-cover duration-500"
@@ -57,8 +52,7 @@ const Slider = () => {
       <div className="hidden group-hover:block absolute top-[60%] md:top-[50%] -translate-x-[0] translate-y-[50%] right-5 text-2xl rounded p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactRight size={40} onClick={nextSlide}/>
       </div>
-      <h2>{slides[currentIndex].title}</h2>
-      <p>{slides[currentIndex].desc}</p>
+      <h2 className="p-4 text-2xl font-raleway">{slides[currentIndex].title}</h2>
     </div>
   );
 };
